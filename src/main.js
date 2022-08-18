4280a5e75e03cc35088885583b955207c8a6e29b
Luis Reyes <59459778+LuisReyes97@users.noreply.github.com>
2022-08-17 23:57:52 -0500

    Luis Reyes
    Configuración de Rutas

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+//DECLARAMOS LAS CONSTANTES
+
+const Home = { template: '<h1>Home</h1>' }
+const About = { template: '<h1>About</h1>' }
+
+//CONFIGURACIÓN -> DEFINIMOS OBJETOS RUTAS
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+
+]
+// CREAMOS OBJETOS RUTAS DE vue Router -> OBJETO DE vue Router
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes, 
+  })
+
+//INSTANCIA DE vue
+  const app = Vue.createApp({})
+  app.use(router)
+  app.mount('#app')
+
