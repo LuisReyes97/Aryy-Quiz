be1c03fb4abb9acb5cc62a7a193a16e1f41ccd7d
Luis Reyes <59459778+LuisReyes97@users.noreply.github.com>
2022-08-17 23:05:13 -0500

    Luis Reyes
    Configuración de la estructura del proyecto

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/reporte-de-salud.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
